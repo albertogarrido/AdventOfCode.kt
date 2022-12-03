24abd896a38515ce10e9ea65af6bec1dee6f331d
Alberto Garrido <hello@albertogarrido.net>
2022-12-03 11:39:17 +0100

    change file template

--- src/Day00.kt
+++ src/Day00.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    runTests(readInput("day0X_test"))
+    runScenario(readInput("daX01"))
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+private fun runScenario(input: List<String>) {
     println(part1(input))
     println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
+
+private fun runTests(testInput: List<String>) {
+    check(part1(testInput) == 1)
 }
